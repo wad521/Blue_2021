5e16dcb196dc62eb8c8e5c3977317ff3294a002e
YangXinglong <1363513229@qq.com>
2021-05-20 23:01:37 +0800

    2021-5-20
    Signed-off-by: YangXinglong <1363513229@qq.com>

--- src/国赛/排列数.java
+++ src/国赛/排列数.java
@@ -1,10 +1,64 @@
 package 国赛;
 
+import java.util.Scanner;
+
 /**
  * @author:yxl
  **/
 public class 排列数 {
+    private static int k, n;
+
+    private static long result = 0;
     public static void main(String[] args) {
-        Scanner input = new Scanner();
+        Scanner input = new Scanner(System.in);
+        n = input.nextInt();
+        k = input.nextInt();
+        int[] num = new int[n];
+        for (int i = 0; i < n; i++) {
+            num[i] = i + 1;
+        }
+        AllPL(num, 0);
+        System.out.println(result);
+    }
+
+
+    public static boolean check(int[] num) {
+        int ans = 0;
+        for (int i = 1; i < n - 1; i++) {
+            if ((num[i] > num[i - 1] && num[i] > num[i + 1]) || (num[i] < num[i - 1] && num[i] < num[i + 1])) {
+                ans++;
+            }
+        }
+        return ans + 1 == k;
+    }
+
+
+    //交换num的i和j的值
+    public static void swap(int[] num, int i, int j) {
+        int t = num[i];
+        num[i] = num[j];
+        num[j] = t;
+    }
+
+    //全排列num数组的下标x及其以后所有元素的全排列
+    //x前的元素全不动。
+    public static void AllPL(int[] num, int x) {
+        if (x == num.length) {
+            //表示依次排列完成
+            if(check(num)){
+                result++;
+            }
+            //返回上一层递归
+            return;
+        } else {
+            for (int i = x; i < num.length; i++) {
+                //交换一次数，表示一种排列可能性
+                swap(num, x, i);
+                //下标为x+1的元素进行递归排列
+                AllPL(num, x + 1);
+                //回溯，遍历下一种可能
+                swap(num, x, i);
+            }
+        }
     }
 }
--- src/国赛/解谜游戏.java
+++ src/国赛/解谜游戏.java
@@ -1,8 +1,45 @@
 package 国赛;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * @author:yxl
  **/
 public class 解谜游戏 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
 
+        for (int i = 0; i < n; i++) {
+            String S3 = input.next();
+            String S2 = input.next();
+            String S1 = input.next();
+            if (check(S1, S2, S3)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+
+    public static boolean check(String S1, String S2, String S3) {
+        char[] s1 = S1.toCharArray();
+        char[] s2 = S2.toCharArray();
+        char[] s3 = S3.toCharArray();
+        for (int i = 0; i < 4; i++) {
+            Map<Character, Integer> map = new HashMap<>();
+            map.put(s1[i], map.getOrDefault(s1[i], 0) + 1);
+            map.put(s2[i], map.getOrDefault(s2[i], 0) + 1);
+            map.put(s2[i + 4], map.getOrDefault(s2[i + 4], 0) + 1);
+            map.put(s3[i], map.getOrDefault(s3[i], 0) + 1);
+            map.put(s3[i + 4], map.getOrDefault(s3[i + 4], 0) + 1);
+            map.put(s3[i + 8], map.getOrDefault(s3[i + 8], 0) + 1);
+            if (map.get('Y') != 1 || map.get('R') != 2 || map.get('G') != 3) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
