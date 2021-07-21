1e21e07111dd44e9ac200dc99bb2c84d109f7d4c
YangXinglong <1363513229@qq.com>
2021-07-21 11:10:31 +0800

    2021.7.21

---
+++ src/剑指Offer/两个链表的第一个公共结点.java
@@ -0,0 +1,29 @@
+package 剑指Offer;
+
+/**
+ * @author:yxl
+ **/
+public class 两个链表的第一个公共结点 {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA, b = headB;
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        return a;
+    }
+}
