29ac02a18cfe41a8e66ff8bf1284b8d530bc119e
YangXinglong <1363513229@qq.com>
2021-09-04 10:59:02 +0800

    斐波那契数列

---
+++ src/剑指Offer/斐波那契数列.java
@@ -0,0 +1,18 @@
+package 剑指Offer;
+
+/**
+ * @author:yxl
+ **/
+public class 斐波那契数列 {
+    //递归
+    static int mod = (int)1e9+7;
+    static int N = 110;
+    static int[] cache = new int[N];
+    public int fib(int n) {
+        if (n <= 1) return n;
+        if (cache[n] != 0) return cache[n];
+        cache[n] = fib(n - 1) + fib(n - 2);
+        cache[n] %= mod;
+        return cache[n];
+    }
+}
