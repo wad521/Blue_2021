dd26ff42bb9392e7f8ed3ea2f80f547ccfea74a5
YangXinglong <1363513229@qq.com>
2021-09-05 17:21:34 +0800

    用random7实现random10

---
+++ src/力扣每日/用random7实现random10.java
@@ -0,0 +1,16 @@
+package 力扣每日;
+
+/**
+ * @author:yxl
+ **/
+public class 用random7实现random10 {
+    public int rand10() {
+        int row, col, idx;
+        do {
+            row = rand7();
+            col = rand7();
+            idx = col + (row - 1) * 7;
+        } while (idx > 40);
+        return 1 + (idx - 1) % 10;
+    }
+}
